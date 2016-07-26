ad96d0b223934d4062bf653b49de6e724790828b
lzhibin <342640194@qq.com>
2016-07-27 07:17:45 +0800

    init

---
+++ shenzhen.py
@@ -0,0 +1,23 @@
+#coding=utf8
+import urllib2
+
+jgcs="http://www.szse.cn/main/disclosure/jgxxgk/jgcs/"   #监管信息公开--监管措施：
+gsgg="http://disclosure.szse.cn/m/drgg.htm"              #上市公司信息--上市公司公告：
+cxda="http://disclosure.szse.cn/m/drgg.htm"              #上市公司信息--上市公司诚信档案：
+jjgg="http://disclosure.szse.cn/m/zqgg.htm"              #基金信息--基金公告/ETF公告：
+zqgg="http://disclosure.szse.cn/m/zqgg.htm"              #债券信息--债券公告：
+ywgg="http://www.szse.cn/main/disclosure/rzrqxx/ywgg/"   #融资融券信息--业务公告：
+jyxx="http://www.szse.cn/main/disclosure/news/dzjy/"     #大宗交易信息--权益类证券大宗交易/债券大宗交易：
+yysg="http://www.szse.cn/main/disclosure/news/yysg/"     #其他交易信息--要约收购：
+class proxy:
+	def __init__(self,proxy_ip):
+		self.proxy=proxy_ip
+	def make_proxy
+		
+		
+class Spyder:
+	def __init__(self):
+		pass
+
+if __name__=='__main__':
+	print 'main'
